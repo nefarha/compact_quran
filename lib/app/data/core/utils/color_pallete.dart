aa3965a9886b1087b122c543b3c9680c62f0d97f
Farhan Febryan <nefarha1@gmail.com>
2024-09-26 15:38:46 +0700

    added color palette

---
+++ lib/app/data/core/utils/color_pallete.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorPallete {
+  static const quran_teal = Color(0xff2e505c);
+  static const quran_gold = Color(0xffF8AA27);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
+import 'package:compact_quran/app/data/core/utils/color_pallete.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
-    GetMaterialApp(
-      title: "Application",
+    CompactQuran(),
+  );
+}
+
+class CompactQuran extends StatelessWidget {
+  const CompactQuran({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Compact Quran",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
+      theme: compactTextTheme(),
+    );
+  }
+}
+
+ThemeData compactTextTheme() {
+  return ThemeData(
+    useMaterial3: false,
+    scaffoldBackgroundColor: ColorPallete.quran_teal,
+    textTheme: GoogleFonts.poppinsTextTheme(),
+    colorScheme: ColorScheme.fromSeed(seedColor: ColorPallete.quran_teal),
   );
 }
